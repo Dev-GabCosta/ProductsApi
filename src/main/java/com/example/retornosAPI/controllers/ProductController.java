232c54b196728426c34f68e744a6c9c374482790
Gabriel Costa <gabriel.costa@zup.com.br>
2025-01-22 15:58:37 -0300

    refactor(ProductController): Modifica controlador para responder com o código 201 quando um objeto for criado

--- src/main/java/com/example/retornosAPI/controllers/ProductController.java
+++ src/main/java/com/example/retornosAPI/controllers/ProductController.java
@@ -5,36 +5,39 @@ import com.example.retornosAPI.services.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
-    private final ProductService service;
+	private final ProductService service;
 
-    public ProductController(ProductService service) {
-        this.service = service;
-    }
+	public ProductController(ProductService service) {
+		this.service = service;
+	}
 
-    @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        return ResponseEntity.ok(service.createProduct(product));
-    }
+	@PostMapping
+	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+		Product createdProduct = service.createProduct(product);
+		URI location = URI.create(String.format("/products/%d", createdProduct.id()));
+		return ResponseEntity.created(location).body(createdProduct);
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        return ResponseEntity.ok(service.getProductById(id));
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+		return ResponseEntity.ok(service.getProductById(id));
+	}
 
-    @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
-        return ResponseEntity.ok(service.getAllProducts());
-    }
+	@GetMapping
+	public ResponseEntity<List<Product>> getAllProducts() {
+		return ResponseEntity.ok(service.getAllProducts());
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        service.deleteProduct(id);
-        return ResponseEntity.noContent().build();
-    }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+		service.deleteProduct(id);
+		return ResponseEntity.noContent().build();
+	}
 }
