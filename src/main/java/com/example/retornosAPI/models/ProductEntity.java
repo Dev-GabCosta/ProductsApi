612cf7e324f9523aa065c51a9d48c2eb24aa9279
Gabriel Costa <gabriel.costa@zup.com.br>
2025-01-22 15:41:00 -0300

    feat(Product, ProductEntity, ProductService): Adicionar campo description ao produto

--- src/main/java/com/example/retornosAPI/models/ProductEntity.java
+++ src/main/java/com/example/retornosAPI/models/ProductEntity.java
@@ -8,44 +8,55 @@ import jakarta.persistence.Id;
 @Entity
 public class ProductEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private Double price;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
 
-    public ProductEntity() {
-    }
+	public ProductEntity() {
+	}
 
-    public ProductEntity(Long id, String name, Double price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
+	public ProductEntity(Long id, String name, String description, Double price) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setPrice(Double price) {
-        this.price = price;
-    }
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Double getPrice() {
-        return price;
-    }
+	public Double getPrice() {
+		return price;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 
 }
