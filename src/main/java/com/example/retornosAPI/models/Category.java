763b8b1036d374dc1bc006690da92ccf263f9cb9
Gabriel Costa <gabriel.costa@zup.com.br>
2025-01-23 15:49:29 -0300

    refactor(Category): Adiciona método check para avaliar a categoria ignorando letras minúsculas

--- src/main/java/com/example/retornosAPI/models/Category.java
+++ src/main/java/com/example/retornosAPI/models/Category.java
@@ -1,9 +1,26 @@
 package com.example.retornosAPI.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Category {
 	ELECTRONICS,
 	FASHION,
 	BOOKS,
 	TOYS,
-	FURNITURE
-}
+	FURNITURE;
+
+	@JsonCreator
+	public static Category checkCategory(String value) {
+
+		for (Category category : Category.values()) {
+
+			if (category.name().equalsIgnoreCase(value)) {
+				return category;
+			}
+
+		}
+
+		throw new IllegalArgumentException("Categoria inválida: " + value);
+	}
+
+	}
